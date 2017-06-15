805b741a1eba146f54729aa291ec714d554fc087
Simon Freedman <simon.freedman@gmail.com>
2017-06-14 21:45:27 -0400

    adding everything into the header file

--- ReadCSV.h
+++ ReadCSV.h
@@ -9,41 +9,91 @@
 #include <algorithm>
 #include <cstring>
 
-class ReadCSV
+namespace read
 {
-public:
-    ReadCSV(const std::string & , const char , char *r);
-    // filename, deliminaator, ignore/remove character
-    ~ReadCSV(){};
     
-    template <class T>
-    T GetValue(const std::string & Key)
+
+    class ReadCSV
     {
-        const std::string & s = Values.at(Key);            
-        std::stringstream   ss(s);
-        T                   t;
-        ss >> t;
-        return t;
+    public:
+        ReadCSV(const std::string & , const char , char *r);
+        // filename, deliminaator, ignore/remove character
+        ~ReadCSV(){};
+        
+        template <class T>
+        T GetValue(const std::string & Key)
+        {
+            const std::string & s = Values.at(Key);            
+            std::stringstream   ss(s);
+            T                   t;
+            ss >> t;
+            return t;
+        };
+        bool ReadLine();
+        inline std::vector<std::string> GetHeaderNames();
+        inline std::string GetLine();
+
+    private:
+        char delim;
+        char *removeChar;
+        std::map<std::string,std::string> Values;
+        std::vector<std::string> HeaderNames;
+        std::fstream data;
+        std::string currentLine;
     };
-    bool ReadLine();
-    inline std::vector<std::string> GetHeaderNames();
-    inline std::string GetLine();
 
-private:
-    char delim;
-    char *removeChar;
-    std::map<std::string,std::string> Values;
-    std::vector<std::string> HeaderNames;
-    std::fstream data;
-    std::string currentLine;
-};
+    inline std::vector<std::string> ReadCSV::GetHeaderNames(){
+        return HeaderNames;
+    }
 
-inline std::vector<std::string> ReadCSV::GetHeaderNames(){
-    return HeaderNames;
+    inline std::string ReadCSV::GetLine(){
+        return currentLine;
+    }
+
+    ReadCSV::ReadCSV( const std::string & fileName 
+        ,const char _delim, char *_r)
+    {
+        /* Constructor for ReadCSV automatically reads in */
+        delim   =       _delim;
+        removeChar =    _r;
+        data.open(fileName);
+        std::string     line;
+        if(!data.is_open()){
+            std::string message ("Could Not Open File");
+            throw message;
+        }
+
+        // Create Header Names from the first line
+        std::getline(data,line);
+        std::stringstream lineStream(line);
+        std::string     cell;
+        while(std::getline(lineStream,cell, delim))
+        {
+            HeaderNames.push_back(cell);
+        }
+    }
+
+    bool ReadCSV::ReadLine()
+    {
+        /* Read in a single line */
+        std::string line;
+        if(std::getline(data,line)){
+            std::stringstream   lineStream(line);
+            std::string         cell;
+            for (int i = 0; i < HeaderNames.size(); i++)
+            {
+                std::getline(lineStream,cell,delim);
+                for(size_t i = 0; i < strlen(removeChar); i++){
+                    cell.erase(std::remove(cell.begin(),cell.end(),removeChar[i]),cell.end());
+                }
+                Values[HeaderNames.at(i)]=cell;
+            }
+            currentLine = line;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
-
-inline std::string ReadCSV::GetLine(){
-    return currentLine;
-}
-
 #endif
